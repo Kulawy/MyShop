c27b794f1aa586c68bb880770819331c8d0d7028
Kulawy <catatau1992@gmail.com>
2018-07-24 21:22:25 +0200

    Add Entity and conect to MS SQL database.

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext: DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> PorductCategories { get; set; }
+
+        // now need to start to migration in View -> Other windows -> Package Manager Console:
+            //Enable-Migrations
+            //Add-Migration InitialMigration
+            //Update-Database
+
+    }
+}

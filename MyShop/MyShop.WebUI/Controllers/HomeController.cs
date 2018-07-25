bbc2bc651ab813ecd2d215a00c453c94af2a75d7
Kulawy <catatau1992@gmail.com>
2018-07-25 13:34:20 +0200

    List  of products on main page Added

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,31 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        private IRepository<Product> context;
+        private IRepository<ProductCategory> productCategoriesContext;
+
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategoriesContext = productCategoryContext;
+
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.ItemsColletion().ToList();
+            return View(products);
+        }
+
+        public ActionResult Detail(string Id)
+        {
+            Product prod = context.Find(Id);
+            if (prod != null)
+                return View(prod);
+            else
+                return HttpNotFound();
+            
         }
 
         public ActionResult About()
